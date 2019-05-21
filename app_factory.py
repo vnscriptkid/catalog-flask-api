28682b549c7c966685a37fb2babdbd0ad80f6324
Thanh Nguyen <vnscriptkid@gmail.com>
2019-05-22 02:01:56 +0700

    add cors

--- app_factory.py
+++ app_factory.py
@@ -1,6 +1,7 @@
 from flask_restplus import Api
 from flask_jwt import JWT
 from flask import Flask
+from flask_cors import CORS
 import os
 
 from controllers.category import api as category_api
@@ -8,6 +9,7 @@ from controllers.article import api as article_api
 from controllers.user import api as user_api
 from security import authenticate, identity
 from config import app_config
+from debug import sql_debug
 from db import db
 
 
@@ -16,10 +18,12 @@ def create_app(config_name):
     app.url_map.strict_slashes = False
     app.config.from_object(app_config[config_name])
     app.secret_key = os.getenv('SECRET_KEY') or 'keep it in ur pocket!'
+    app.after_request(sql_debug)
+    CORS(app)
 
     api = Api(app, title="Restful API", description="Blogging App")
-    register_api(api)
 
+    register_api(api)
     init_extensions(app)
 
     return app
